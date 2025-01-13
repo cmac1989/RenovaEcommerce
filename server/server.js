ac399e16a56636e2bc325a3a40f1c0e870537cf3
Cameron <w0796224@myscc.ca>
2025-01-12 23:12:43 -0500

    Included db connection in server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const db = require("../server/config/database");
+
 require('dotenv').config();
 
 const app = express();
+
+db.connect();
 
 // Middleware
 app.use(cors());
