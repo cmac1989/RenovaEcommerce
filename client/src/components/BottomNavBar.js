886e0a0946b63b0533e8e27d661895c3b70c7388
Wania Sharif <w0842810@myscc.ca>
2025-01-24 10:06:37 -0500

    #29 Create initial files for bottom nav

---
+++ client/src/components/BottomNavBar.js
@@ -0,0 +1,12 @@
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+function BottomNavBar() {
+  return (
+    <div className="bottomNav">
+        
+    </div>
+  );
+}
+
+export default BottomNavBar
