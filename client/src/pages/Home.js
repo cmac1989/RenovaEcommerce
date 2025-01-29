cd7060ec9c60a5aa28aac904b46506aee1ddd534
Wania Sharif <w0842810@myscc.ca>
2025-01-29 12:03:48 -0500

    #46 import, display, and format images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../styles/homePage.css'
+import '../assets/images/IMG_8511.jpg'
+import '../assets/images/IMG_8509.jpg'
 
 function Home() {
+  const img = require('../assets/images/IMG_8511.jpg')
+  const img2 = require('../assets/images/IMG_8509.jpg')
   return (
     <div className="home-page">
-        <h1>Home</h1>
+        <div className="section a">
+          <img src={img} />
+          <img src={img2} />
+        </div>
+
+        <div className="section b">
+
+        </div>
     </div>
   )
 }
