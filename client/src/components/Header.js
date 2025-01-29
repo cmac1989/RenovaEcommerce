6500c5f5f04f025b3dc9d69a0190ccdc43c59693
Wania Sharif <w0842810@myscc.ca>
2025-01-29 14:00:21 -0500

    #23 Change header layout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,14 +11,14 @@ function Header() {
   const cartQuantity = useSelector(state => state.cart.totalAmount)
   return (
     <div className="header">
-        <div className="banner">PLACEHOLDER</div>
+        {/* <div className="banner">PLACEHOLDER</div> */}
         <div className="headerContent">
+          <div className="headerLogo">LOGO</div>
             <div className="headerNav">
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/products'>Products</NavLink>
                 <NavLink to='/contact'>Contact</NavLink>
             </div>
-            <div className="headerLogo">LOGO</div>
             <div className="headerNav2">
                 <NavLink>Sign in</NavLink>
                 <NavLink to='/cart'> {cartQuantity} {<CgShoppingCart />}</NavLink>
