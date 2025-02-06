a9f7b53a9b215811207ec570eea1f0fc3345e739
Wania Sharif <w0842810@myscc.ca>
2025-02-05 21:55:52 -0500

    #23 add logo to header + fix header sizing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,13 +2,18 @@ import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import '../styles/header.css'
 import { CgShoppingCart } from 'react-icons/cg'
+import '../assets/images/logo.png'
 
 function Header() {
-  const cartQuantity = useSelector(state => state.cart.totalAmount)
+  const cartQuantity = useSelector(state => state.cart.totalAmount);
+  const logo = require('../assets/images/logo.png');
   return (
     <div className="header">
         <div className="headerContent">
-          <div className="headerLogo">LOGO</div>
+          <div className="headerLogo">
+            <img src={logo} alt=""/>
+            RENOVA
+            </div>
             <div className="headerNav">
                 <NavLink to='/'>HOME</NavLink>
                 <NavLink to='/products'>PRODUCTS</NavLink>
